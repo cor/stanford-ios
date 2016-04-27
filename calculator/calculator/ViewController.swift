afe89f2b245f2d29641c9d2cc15aadda022d266c
cor <cor@pruijs.nl>
2016-04-27 17:13:00 +0200

    Add asic UI to calculator

--- calculator/calculator/ViewController.swift
+++ calculator/calculator/ViewController.swift
@@ -10,16 +10,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var displayLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func operandButtonPressed(sender: UIButton) {
+        displayLabel.text = sender.currentTitle!
     }
-
-
+    
+    @IBAction func operatorButtonPressed(sender: UIButton) {
+    
+    }
+    
 }
 
