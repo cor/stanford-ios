8907ed2c4db2bca3c91904b44d865a927b69a076
cor <cor@pruijs.nl>
2016-04-27 16:45:03 +0200

    Add empty calculator project

---
+++ calculator/calculator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  calculator
+//
+//  Created by Cor Pruijs on 27-04-16.
+//  Copyright © 2016 CorCoder. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
