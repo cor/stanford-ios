dba60bf7ea874fa46ee68f15e8f2c2a186ba951a
cor <cor@pruijs.nl>
2016-05-20 14:45:55 +0200

    Improve FaceIt

---
+++ FaceIt/FaceIt/FaceViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  FaceIt
+//
+//  Created by Cor Pruijs on 17-05-16.
+//  Copyright © 2016 CorCoder. All rights reserved.
+//
+
+import UIKit
+
+class FaceViewController: UIViewController {
+
+    
+
+}
+
